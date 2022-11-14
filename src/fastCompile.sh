fc04dd469f654f7c923adf47c16297c6eafbcb9f
nanLianQing <71924344+f401@users.noreply.github.com>
2022-11-14 09:54:01 +0800

    Update and rename compile.sh to fastCompile.sh

--- src/fastCompile.sh
+++ src/fastCompile.sh
@@ -8,6 +8,6 @@ gcc -c `find $PROJECT_ROOT_PATH -name "*.c"` ${COMPILER_OPTIONS}
  
 g++ -c `find $PROJECT_ROOT_PATH -name "*.cpp"` ${COMPILER_OPTIONS}
 
-g++ `find $PROJECT_ROOT_PATH -name "*.o"` -o main -lstdc++ ${LINK_OPTIONS} ${COMPILER_OPTIONS}
+g++ `find $PROJECT_ROOT_PATH -name "*.o"` -o gameSL -lstdc++ ${LINK_OPTIONS} ${COMPILER_OPTIONS}
 
 rm -f `find $PROJECT_ROOT_PATH -name "*.o"`
