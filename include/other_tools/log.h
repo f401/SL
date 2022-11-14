6c3bb902d64ac2c696c2152f8a2f35c5b2560ab7
nanLianQing <71924344+f401@users.noreply.github.com>
2022-11-14 15:47:14 +0800

    Update log.h

--- include/other_tools/log.h
+++ include/other_tools/log.h
@@ -5,8 +5,8 @@
 extern "C" {
 #endif
 
-#define sl_logger_info(text,...) (sl_logger_log_info(text, __func__, __LINE__, __FILE_NAME__, ##__VA_ARGS__)) 
-#define sl_logger_error(text,...) (sl_logger_log_error(text, __func__, __LINE__, __FILE_NAME__, ##__VA_ARGS__)) 
+#define sl_logger_info(text,...) (sl_logger_log_info(text, __func__, __LINE__, __FILE__, ##__VA_ARGS__)) 
+#define sl_logger_error(text,...) (sl_logger_log_error(text, __func__, __LINE__, __FILE__, ##__VA_ARGS__)) 
 
 void sl_logger_log_info(const char* text, const char* funcName, unsigned int line, const char* fileName, ...);
 void sl_logger_log_error(const char* text, const char* funcName, unsigned int line, const char* fileName, ...);
